79369d805277bb1e7c602fc816dc1baf6753843b
Nony <john2015084@gmail.com>
2026-03-05 08:51:57 +0100

    fix cors config

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.set('view engine', 'ejs');
 dotenv.config();
 app.use(express.urlencoded({limit:"5mb" ,extended:true}));
 app.use(express.json({limit:"50mb"}));
-app.use(cors());
+app.use(cors({
+    origin:["https://amebonaija.vercel.app", "http://localhost:5173"],
+    credentials: true
+}));
 const UserRouter =require("./routers/user.routes");
 const PostRouter =require("./routers/post.routes");
 const authRoutes = require("./routers/authRoutes");
