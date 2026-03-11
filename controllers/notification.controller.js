891808cbcbf9af9b9ba454dbd6468b1d19c19898
Nony-montana <john2015084@gmail.com>
2026-03-11 15:38:46 +0100

    Changed id to _id

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -5,7 +5,7 @@ const NotificationModel = require("../models/notification.model");
 // GET all notifications for logged-in user
 const getNotifications = async (req, res) => {
   try {
-    const notifications = await NotificationModel.find({ recipient: req.user.id })
+    const notifications = await NotificationModel.find({ recipient: req.user._id })
       .sort({ createdAt: -1 })
       .limit(20)
       .populate("sender", "firstName lastName");
@@ -29,7 +29,7 @@ const markAsRead = async (req, res) => {
 // MARK all notifications as read
 const markAllAsRead = async (req, res) => {
   try {
-    await NotificationModel.updateMany({ recipient: req.user.id, isRead: false }, { isRead: true });
+    await NotificationModel.updateMany({ recipient: req.user._id, isRead: false }, { isRead: true });
     res.status(200).json({ success: true, message: "All marked as read" });
   } catch (err) {
     res.status(500).json({ success: false, message: "Failed to mark all as read" });
