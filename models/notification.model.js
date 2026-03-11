f21f596d1ea4b0462049ce0281ed45135239c3c1
Nony-montana <john2015084@gmail.com>
2026-03-11 16:21:05 +0100

    Updated user in notification model

--- models/notification.model.js
+++ models/notification.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
   {
-    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
     type: {
       type: String,
       enum: ["like", "comment", "approved", "rejected", "new_post"],
